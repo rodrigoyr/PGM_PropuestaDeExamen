2ce69e19393723bf8e7a65f21d07e9ac081f0208
rodrigoyr <ryeperub@myuax.com>
2023-11-07 11:28:04 +0100

    Guardar cambios commit

--- main.cpp
+++ main.cpp
@@ -1,33 +1,23 @@
+// Path: main.cpp
 #include <stdio.h>
-#include <math.h>
+
+// Path: main.cpp
 #include <stdio.h>
-#include <math.h>
+
+// Path: main.cpp
+#include <stdio.h>
 
 int main() {
-    int a, b, c;
-    int maxValor;
+    int i, j, k;
 
-    // Paso 1: Solicita al usuario que ingrese el valor máximo para buscar triples pitagóricos.
-    printf("Paso 1: Ingresa el valor máximo para buscar triples pitagóricos: ");
-    scanf("%d", &maxValor);
+    // Paso 1: Utiliza un bucle para iterar a través de los valores de 1 a 10.
+    for (i = 1; i <= 10; i++) {
+        // Paso 2: Utiliza un segundo bucle para iterar a través de los valores de 1 a 10.
+        for (j = 1; j <= 10; j++) {
+            // Paso 3: Calcula el producto de i y j.
+            k = i * j;
 
-    printf("Paso 2: Triples Pitagóricos:\n");
-
-    // Paso 3: Utiliza un bucle para iterar a través de valores potenciales para "a".
-    for (a = 1; a <= maxValor; a++) {
-        // Paso 4: Dentro del primer bucle, utiliza un segundo bucle para iterar a través de valores potenciales para "b".
-        for (b = a; b <= maxValor; b++) {
-            // Paso 5: Calcula el tercer lado del triángulo (c) usando la fórmula c = sqrt(a^2 + b^2).
-            c = sqrt(a * a + b * b);
-
-            // Paso 6: Verifica si los tres lados forman un triple pitagórico.
-            if (c * c == a * a + b * b) {
-                // Imprime el triple pitagórico.
-                printf("Triple Pitagórico: (%d, %d, %d)\n", a, b, c);
-            }
+            // Paso 4: Imprime el producto.
+            printf("%d * %d = %d\n", i, j, k);
         }
-    }
-
-    return 0;
-}
-
+    }       }

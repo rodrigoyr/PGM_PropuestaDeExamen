9c72430204977b9dd8b82782fc2b1cbc5e66006b
rodrigoyr <ryeperub@myuax.com>
2023-11-07 11:33:58 +0100

    Trying final commit

--- main.cpp
+++ main.cpp
@@ -2,14 +2,14 @@
 #include <cmath>
 
 int main() {
-    int max_valor = 100; // Puedes ajustar el valor máximo según tus necesidades
+    int max_valor = 100;
 
     for (int a = 1; a <= max_valor; a++) {
         for (int b = a; b <= max_valor; b++) {
             double c = std::sqrt(a * a + b * b);
 
             if (c == std::round(c) && a + b > c) {
-                std::cout << "Triple pitagórico: " << a << ", " << b << ", " << static_cast<int>(c) << std::endl;
+                std::cout << "Triple pitagorico: " << a << ", " << b << ", " << static_cast<int>(c) << std::endl;
             }
         }
     }
